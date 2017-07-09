6b3737e5f7bae66093c8f0bc6c46c2414338af47
ankitbose818 <ankit_dipendubose@srmuniv.edu.in>
2017-07-09 14:38:54 +0530

    Add files via upload

---
+++ number.java
@@ -0,0 +1,26 @@
+* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args)
+	{
+		int num=6;
+		if(num>0)
+		{
+			System.out.println("positive");
+		}
+			else if(num<0)
+			{
+				System.out.println("Negative");
+			}
+			else
+			{
+				System.out.println("Zero");
+		}
+	}
+}
