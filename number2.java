14d89aa6115cc665258f4788497620f55b885773
ankitbose818 <ankit_dipendubose@srmuniv.edu.in>
2017-07-09 15:12:42 +0530

    Add files via upload

---
+++ number2.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 char val='a';
+	if (val=='a' || val=='e' || val=='i' || val=='o' || val=='u')
+	System.out.println("the characters are vowels");
+	else
+	System.out.println("CONSONANTS");
+		}
+}
